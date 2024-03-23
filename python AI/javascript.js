d9b2ef247c0f0cffa446b59741a622805dd19995
NOTYOUONI1 <sorry56363472@gmail.com>
2024-03-23 20:08:23 +0600

    3/23

--- python AI/javascript.js
+++ python AI/javascript.js
@@ -1,11 +1,1 @@
-let op = [1,2,3,4,5,6,7,8,9];
-//          0         1         2            3         4       5       6
-let ar = ['spiderman', 'ironman']
-let po = ['hozor']
-//let i = op.concat(ar, po)
-ar.unshift(po)
-let hi = ar.splice()
-//console.log(op, '\n', length.op)
-op.splice(1, 2, 'rakib')
-let company = ['BLOOMBBERG', 'GOOGLE', 'MICROSOFT', 'UBER', 'IBM', 'NETFLIX']
-company.splice(6, 0, 'Amazon')
+console.log('rakib')
--- python AI/python ai 1.py
+++ python AI/python ai 1.py
@@ -1,8 +1,66 @@
-class student :
-    def __init__(self,fullname,mask):
-        self.name = fullname
-        self.mask = mask
-        print(self.name,mask)
+from ast import Num
+from numbers import Number
+import time
+times = time.time()
+"""
+name = str(input('enter your name :'))
+
+math = int(input("enter math result :"))
+
+english = int(input("enter english result :"))
+
+class student:
+    def __init__(self,name,math_mask,english_mask):
+        self.name = name
+        self.math_mask = math_mask
+        self.english_mask = english_mask
+        print('my name is =',self.name,'math result',self.math_mask,'english result',self.english_mask)
         
-s1 = student('rakib', 69)
-s2 = student('sakib', 98)
+    @staticmethod
+    def college ():
+        print('rakinnnnnnnnnnnn')    
+        
+student(name, math, english)
+
+student.college()"""
+
+
+
+'''class bank:
+    def bank (self):    
+        self.bank_name = 'RRR'
+        self.bank_amount = 55555555555,'Billon'
+        #print('bank name :',self.bank_name,'money',self.bank_amount)
+        pas = int(input("enter password :"))
+        pj= 1234
+        if(pas == pj):
+            print('bank name :',self.bank_name,'money',self.bank_amount)
+        else:
+            print("rong pin")    
+
+
+
+p = bank()
+print(p.bank())'''
+#email_input = input("enter yur email addrese :")
+
+email_inputp = str(input("enter yur email addrese :"))
+
+h = email_inputp.isalpha
+
+print(h)
+
+class email:
+    def comdition (self,email_input):
+        if len(email_input) > 16:
+            if email_input == email_input :
+                print("p")
+            else:
+                print("this email is unvarifid")
+        else:
+            print("this email is unvarifid")    
+
+
+
+timel = time.time()
+print(timel)
